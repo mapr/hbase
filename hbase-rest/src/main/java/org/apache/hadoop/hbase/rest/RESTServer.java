c345baa486f4d8a6d4d8a651d92a28417bb981b7
Mykhailo Kysliuk <mkysliuk@mapr.com>
2019-01-25 20:48:53 +0200

    MAPR-HBASE-88: Endpoint /status/cluster returns list tables instead cluster status

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/RESTServer.java
@@ -183,20 +183,6 @@ public class RESTServer implements Constants {
       ResourceConfig.class.getCanonicalName());
     sh.setInitParameter("com.sun.jersey.config.property.packages",
       "jetty");
-    // The servlet holder below is instantiated to only handle the case
-    // of the /status/cluster returning arrays of nodes (live/dead). Without
-    // this servlet holder, the problem is that the node arrays in the response
-    // are collapsed to single nodes. We want to be able to treat the
-    // node lists as POJO in the response to /status/cluster servlet call,
-    // but not change the behavior for any of the other servlets
-    // Hence we don't use the servlet holder for all servlets / paths
-    ServletHolder shPojoMap = new ServletHolder(containerClass);
-    @SuppressWarnings("unchecked")
-    Map<String, String> shInitMap = sh.getInitParameters();
-    for (Entry<String, String> e : shInitMap.entrySet()) {
-      shPojoMap.setInitParameter(e.getKey(), e.getValue());
-    }
-    shPojoMap.setInitParameter(JSONConfiguration.FEATURE_POJO_MAPPING, "true");
 
     // set up Jetty and run the embedded server
 
@@ -235,7 +221,6 @@ public class RESTServer implements Constants {
     server.setStopAtShutdown(true);
       // set up context
     Context context = new Context(server, "/", Context.SESSIONS);
-    context.addServlet(shPojoMap, "/status/cluster");
     context.addServlet(sh, "/*");
     if (authFilter != null) {
       context.addFilter(authFilter, "/*", 1);
